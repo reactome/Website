35d52b744751f561361953eea895f11c000ad438
gviteri <gviteri@ebi.ac.uk>
2018-12-18 10:27:17 +0000

    aws progress

--- scripts/joomla_migration.sh
+++ scripts/joomla_migration.sh
@@ -92,11 +92,13 @@ normalise_owner_and_permissions () {
     SERVER="${RELEASE_SERVER}.reactome.org"
     # make sure files have set user and group before moving them (SOURCE)
     echo "Updating file's owner in the source server [${RELEASE_SERVER}] before synchronisation."
-    echo ${SRCOSPASSWD}
-    echo ${SRCOSUSER}
-    echo ${PASSPHRASE}
 
-    sshpass -p ${SRCOSPASSWD} sudo -u ${SRCOSUSER} chown -R ${OWNER} ${_JOOMLA_STATIC} #&> /dev/null
+    #www-data ALL=(ALL) NOPASSWD:/var/www/html/test.sh
+    #echo ${SRCOSPASSWD}
+    #echo ${SRCOSUSER}
+    #echo ${PASSPHRASE}
+
+    sshpass -P passphrase -f <(printf '%s\n' ${PASSPHRASE}) ssh -i ${PRIVATE_KEY} -qn -o StrictHostKeyChecking=no -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -t "shared"@${SERVER} "echo ${SRCOSPASSWD} | sudo -u ${SRCOSUSER} chown -R ${OWNER} ${_JOOMLA_STATIC}" #&> /dev/null
     OUT=$?
     if [[ "$OUT" -ne 0 ]]; then
         echo "[ERROR] Couldn't normalise the owner (${OWNER}) of the static folder ${_JOOMLA_STATIC} in the Source server [${SERVER}]"
