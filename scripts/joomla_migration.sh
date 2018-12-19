51e44a80dc0cbbf8c127251410aabecde1d3a2e9
gviteri <gviteri@ebi.ac.uk>
2018-12-19 11:37:53 +0000

    aws in progress

--- scripts/joomla_migration.sh
+++ scripts/joomla_migration.sh
@@ -93,7 +93,8 @@ normalise_owner_and_permissions () {
     # make sure files have set user and group before moving them (SOURCE)
     echo "Updating file's owner in the source server [${RELEASE_SERVER}] before synchronisation."
 
-    sudo -S ${SYNC_SCRIPTS_HOME}/website_chown.sh
+    #sudo -S ${SYNC_SCRIPTS_HOME}/website_chown.sh
+    sudo -u gviteri chown -R www-data:reactome ${_JOOMLA_STATIC}
 
     OUT=$?
     if [[ "$OUT" -ne 0 ]]; then
