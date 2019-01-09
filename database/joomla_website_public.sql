dcbd5b5c7ed8c13de9d9cefec80fdc27a7980321
reactomebot <reactome@gmail.com>
2019-01-09 03:00:05 +0000

    website2github.sh automated commit on 2019-01-09 03:00:01 UTC

--- database/joomla_website_public.sql
+++ database/joomla_website_public.sql
@@ -2377,7 +2377,7 @@ CREATE TABLE `rlp_user_keys` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `series` (`series`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
