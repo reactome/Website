4ceb0bf1ae6f5e496dab04966aa7fdd7a77555cb
gviteri <gviteri@ebi.ac.uk>
2019-01-16 15:26:55 +0000

    send file to destination

--- scripts/joomla_migration.sh
+++ scripts/joomla_migration.sh
@@ -57,6 +57,7 @@ RELEASE_SERVER="release.reactome.org"
 PRD1_SERVER="reactome.org"
 
 SYNC_CHOWN="${_JOOMLA_STATIC}/scripts/website_chown.sh";
+SYNC_CHOWN_DEST="/tmp/website_chown.sh";
 
 OWNER="www-data:reactome"
 
@@ -115,7 +116,7 @@ normalise_owner_and_permissions () {
 normalise_owner_permissions_and_flags_remote () {
     echo "Updating file's owner in [${DEST_SERVER}]. Executing ${SYNC_CHOWN}."
 
-    sshpass -P passphrase -f <(printf '%s\n' ${PASSPHRASE}) rsync -rogtO -e 'ssh -i '${PRIVATE_KEY}' -o StrictHostKeyChecking=no -o LogLevel=quiet -o UserKnownHostsFile=/dev/null' -i --links --delete --ignore-errors ${SYNC_CHOWN} ${SHARED_USER}@${SERVER}:${SYNC_CHOWN} #2> /dev/null
+    sshpass -P passphrase -f <(printf '%s\n' ${PASSPHRASE}) rsync -rogtO -e 'ssh -i '${PRIVATE_KEY}' -o StrictHostKeyChecking=no -o LogLevel=quiet -o UserKnownHostsFile=/dev/null' -i --links --delete --ignore-errors ${SYNC_CHOWN} ${SHARED_USER}@${SERVER}:${SYNC_CHOWN_DEST} #2> /dev/null
 
     # sudo visudo - authorise shared user to run chown script without asking for password
     sshpass -P passphrase -f <(printf '%s\n' ${PASSPHRASE}) ssh -i ${PRIVATE_KEY} -n -o StrictHostKeyChecking=no -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -t ${SHARED_USER}@${DEST_SERVER} "sudo ${SYNC_CHOWN}"
