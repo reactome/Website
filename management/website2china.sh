95c46e94402b92716ca7c8247bdb42af632de05b
gviteri <gviteri@ebi.ac.uk>
2019-01-17 17:16:07 +0000

    adjusting to aws config

--- management/website2china.sh
+++ management/website2china.sh
@@ -23,11 +23,7 @@
 
 # Don't need to execute as sudo
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd ${DIR}
-
 PRIVATE_FOLDER="/usr/local/reactomes/Reactome/production/Website/private"
-
 LOCAL_REPO="/usr/local/reactomes/Reactome/production/Website/static"
 
 MYSQL=`which mysql`
@@ -54,14 +50,14 @@ echo "Creating temporary database [${TEMP_DBNAME}]"
 CREATE_TEMP_DB="CREATE DATABASE IF NOT EXISTS ${TEMP_DBNAME} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
 ${MYSQL} -u ${DBUSER} -e "${CREATE_TEMP_DB}"
 OUT=$?
-if [ "$OUT" -ne 0 ]; then
+if [[ "$OUT" -ne 0 ]]; then
     echo "[ERROR] Could not temporary create database [${TEMP_DBNAME}]"
 fi
 
 echo "Importing into database [$TEMP_DBNAME]"
 ${MYSQL} -u ${DBUSER} ${TEMP_DBNAME} < ${PRIVATE_DB_FILE}
 OUT=$?
-if [ "$OUT" -ne 0 ]; then
+if [[ "$OUT" -ne 0 ]]; then
     echo "[ERROR] Could not IMPORT file [$PRIVATE_DB_FILE] to the database [$TEMP_DBNAME]"
 fi
 
@@ -78,34 +74,38 @@ SQL_CL_USERS="UPDATE rlp_users SET username = concat('guest',id), name = concat(
 SQL_CL_ADMIN="UPDATE rlp_users SET username = 'admin', name = 'Joomla Admin', email = 'change-me@nodomain.com', requireReset = 0, resetCount = 0, params = '{\"admin_style\":\"\",\"admin_language\":\"\",\"language\":\"\",\"editor\":\"\",\"helpsite\":\"\",\"timezone\":\"\"}', registerDate='2017-01-01 09:00:00', lastvisitDate='2017-01-01 10:00:00', lastResetTime='0000-00-00 00:00:00', password = '${ADMIN_PASSWD}' WHERE id = ${SUPER_USER_ID};"
 SQL_DELETE_GROUPS="DELETE FROM rlp_user_usergroup_map WHERE user_id <> ${SUPER_USER_ID} AND group_id <> 2;"
 SQL_DELETE_EJBBKP="DELETE FROM rlp_easyjoomlabackup;"
-SQL_SET_AI_EJBBKP="ALTER TABLE rlp_easyjoomlabackup AUTO_INCREMENT = 1;"
+SQL_SET_AI_EJBBKP="SET sql_mode = '';ALTER TABLE rlp_easyjoomlabackup AUTO_INCREMENT = 1;"
 SQL_DELETE_SESSIONS="DELETE FROM rlp_session;"
-SQL_UPDT_ARTICLE="UPDATE rlp_content SET hits=0,checked_out=0,checked_out_time='0000-00-00 00:00:00',version=1, created_by='${SUPER_USER_ID}',modified_by='${SUPER_USER_ID}',created='2017-01-01 09:00:00',modified='2017-01-01 09:00:00' WHERE catid <> 26;" # not in news category
-SQL_UPDT_SITE_LAST_CHECK="UPDATE rlp_update_sites SET last_check_timestamp='0000-00-00 00:00:00';"
+SQL_UPDT_ARTICLE="SET sql_mode = '';UPDATE rlp_content SET hits=0,checked_out=0,checked_out_time='0000-00-00 00:00:00',version=1, created_by='${SUPER_USER_ID}',modified_by='${SUPER_USER_ID}',created='2017-01-01 09:00:00',modified='2017-01-01 09:00:00' WHERE catid <> 26;" # not in news category
+SQL_UPDT_SITE_LAST_CHECK="SET sql_mode = '';UPDATE rlp_update_sites SET last_check_timestamp='0000-00-00 00:00:00';"
 SQL_DELETE_UPDATES="DELETE FROM rlp_updates;"
-SQL_SET_AI_UPDATES="ALTER TABLE rlp_updates AUTO_INCREMENT = 1;"
-SQL_UPDT_TAGS="UPDATE rlp_tags SET hits=0,checked_out=0,checked_out_time='0000-00-00 00:00:00';"
+SQL_SET_AI_UPDATES="SET sql_mode = '';ALTER TABLE rlp_updates AUTO_INCREMENT = 1;"
+SQL_UPDT_TAGS="SET sql_mode = '';UPDATE rlp_tags SET hits=0,checked_out=0,checked_out_time='0000-00-00 00:00:00';"
 SQL_DEL_UCM_CONTENT="DELETE FROM rlp_ucm_content;"
-SQL_SET_AI_UCM_CONTENT="ALTER TABLE rlp_ucm_content AUTO_INCREMENT = 1;"
+SQL_SET_AI_UCM_CONTENT="SET sql_mode = '';ALTER TABLE rlp_ucm_content AUTO_INCREMENT = 1;"
 SQL_DEL_UCM_HISTORY="DELETE FROM rlp_ucm_history;"
-SQL_SET_AI_UCM_HISTORY="ALTER TABLE rlp_ucm_history AUTO_INCREMENT = 1;"
-SQL_UPDT_CONTENTITEM_TAG_MAP="UPDATE rlp_contentitem_tag_map SET tag_date='0000-00-00 00:00:00';"
+SQL_SET_AI_UCM_HISTORY="SET sql_mode = '';ALTER TABLE rlp_ucm_history AUTO_INCREMENT = 1;"
+SQL_UPDT_CONTENTITEM_TAG_MAP="SET sql_mode = '';UPDATE rlp_contentitem_tag_map SET tag_date='0000-00-00 00:00:00';"
 SQL_CLEAN_WHAT_IS_NOT_PUBLIC="UPDATE rlp_content SET introtext='' WHERE access <> 1;"
-SQL_NORMALISE_MODULES="UPDATE rlp_content SET checked_out=0,checked_out_time='0000-00-00 00:00:00';"
+SQL_NORMALISE_MODULES="SET sql_mode = '';UPDATE rlp_content SET checked_out=0,checked_out_time='0000-00-00 00:00:00';"
 SQL_EJB_CLEAN_TOKEN="UPDATE rlp_extensions SET params='{\"token\":\"\",\"type\":\"1\",\"donation_code\":\"\"}' WHERE extension_id = 10209;"
-SQL_REMOVE_BANNER="UPDATE rlp_modules SET published=0,publish_up='0000-00-00 00:00:00',publish_down='0000-00-00 00:00:00',content='' WHERE id = 581;"
+SQL_REMOVE_BANNER="SET sql_mode = '';UPDATE rlp_modules SET published=0,publish_up='0000-00-00 00:00:00',publish_down='0000-00-00 00:00:00',content='' WHERE id = 581;"
 SQL_DEL_USER_KEYS="DELETE FROM rlp_user_keys;"
 SQL_DEL_POST_INSTALL_MSG="DELETE FROM rlp_postinstall_messages;"
-SQL_SET_AI_PIM="ALTER TABLE rlp_postinstall_messages AUTO_INCREMENT = 1;"
+SQL_SET_AI_PIM="SET sql_mode = '';ALTER TABLE rlp_postinstall_messages AUTO_INCREMENT = 1;"
 SQL_DEL_MSG="DELETE FROM rlp_messages;"
-SQL_SET_AI_MSG="ALTER TABLE rlp_messages AUTO_INCREMENT = 1;"
+SQL_SET_AI_MSG="SET sql_mode = '';ALTER TABLE rlp_messages AUTO_INCREMENT = 1;"
 SQL_REMOVE_TWEETS="UPDATE rlp_modules SET published=0 WHERE id = 496;"
 SQL_REMOVE_WHYREACTOME="UPDATE rlp_modules SET published=0 WHERE id = 505;"
 SQL_ADD_CHINA_WHYREACTOME="UPDATE rlp_modules SET published=1 WHERE id = 582;"
 SQL_REMOVE_STAFF_MENU="UPDATE rlp_menu SET published=0 WHERE id = 284;"
+SQL_DEL_ACT_LOGS="DELETE FROM rlp_action_logs;"
+SQL_SET_AI_ACT_LOGS="SET sql_mode = '';ALTER TABLE rlp_action_logs AUTO_INCREMENT = 1;"
+SQL_DEL_PVT_REQS="DELETE FROM rlp_privacy_requests;"
+SQL_SET_AI_PVT_REQS="SET sql_mode = '';ALTER TABLE rlp_privacy_requests AUTO_INCREMENT = 1;"
 
 # security check, must never have, it is here just in case
-if [ ${DBNAME} == ${TEMP_DBNAME} ]; then
+if [[ ${DBNAME} == ${TEMP_DBNAME} ]]; then
     echo "DB and TEMP DB are the same. A clean up is done directly in TEMP, having the same as DB a disaster will happen."
 fi
 
@@ -141,14 +141,13 @@ SQL_COMMANDS+=( "${SQL_REMOVE_WHYREACTOME}" )
 SQL_COMMANDS+=( "${SQL_ADD_CHINA_WHYREACTOME}" )
 SQL_COMMANDS+=( "${SQL_REMOVE_STAFF_MENU}" )
 
-
 for key in "${!SQL_COMMANDS[@]}"
 do
     SQL=${SQL_COMMANDS[${key}]}
     echo "  ${key})" ${SQL_COMMANDS[${key}]} | cut -c1-50
     ${MYSQL} -u ${DBUSER} ${TEMP_DBNAME} -e "${SQL}"
     OUT=$?
-    if [ "$OUT" -ne 0 ]; then
+    if [[ "$OUT" -ne 0 ]]; then
         echo "[ERROR] Could not run [${SQL}]"
     fi
 done
@@ -156,7 +155,7 @@ done
 echo "Dumping CLEANED Joomla MySQL database - [$TEMP_DBNAME]"
 ${MYSQL_DUMP} --skip-dump-date -u ${DBUSER} ${TEMP_DBNAME} > ${PUBLIC_DB_FILE}
 OUT=$?
-if [ "$OUT" -ne 0 ] || [ ! -s ${PUBLIC_DB_FILE} ]; then
+if [[ "$OUT" -ne 0 ]] || [[ ! -s ${PUBLIC_DB_FILE} ]]; then
     echo "[ERROR] Could not DUMP public Joomla Database"
 fi
 
@@ -164,7 +163,7 @@ echo "Dropping temporary database - [$TEMP_DBNAME]"
 SQL_DROP_TEMP_DB="DROP DATABASE ${TEMP_DBNAME};"
 ${MYSQL} -u ${DBUSER} -e "${SQL_DROP_TEMP_DB}";
 OUT=$?
-if [ "$OUT" -ne 0 ]; then
+if [[ "$OUT" -ne 0 ]]; then
     echo "[ERROR] Could not DROP temporary database ${TEMP_DBNAME}"
 fi
 
