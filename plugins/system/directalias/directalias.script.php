a7752db231f8781911500573e92a111ec90d18bb
Guilherme Viteri <gviteri@ip-172-31-30-104.eu-west-2.compute.internal>
2019-01-18 14:31:02 +0000

    joomla 3.9.2

--- plugins/system/directalias/directalias.script.php
+++ plugins/system/directalias/directalias.script.php
@@ -1,26 +1,41 @@
 <?php
 /**
-* @package		Direct Alias
-* @copyright	Copyright (C) 2009-2013 AlterBrains.com. All rights reserved.
-* @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
-*/
+ * @package        Direct Alias
+ * @copyright      Copyright (C) 2009-2017 AlterBrains.com. All rights reserved.
+ * @license        http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
 
-defined('_JEXEC') or die('Restricted access'); 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * @since 1.0
+ */
 class plgSystemDirectaliasInstallerScript
 {
-	public function install($parent)
+	/**
+	 * @since 1.0
+	 */
+	public function install()
 	{
-		JFactory::getApplication()->enqueueMessage(JText::_('Successfully installed "System - Direct Alias" plugin!'));
+		Factory::getApplication()->enqueueMessage(Text::_('Successfully installed "System - Direct Alias" plugin!'));
 	}
 
-	public function uninstall($parent)
+	/**
+	 * @since 1.0
+	 */
+	public function uninstall()
 	{
-		JFactory::getApplication()->enqueueMessage(JText::_('Successfully uninstalled "System - Direct Alias" plugin!'));
+		Factory::getApplication()->enqueueMessage(Text::_('Successfully uninstalled "System - Direct Alias" plugin!'));
 	}
 
-	public function update($parent)
+	/**
+	 * @since 1.0
+	 */
+	public function update()
 	{
-		JFactory::getApplication()->enqueueMessage(JText::_('Successfully updated "System - Direct Alias" plugin!'));
+		Factory::getApplication()->enqueueMessage(Text::_('Successfully updated "System - Direct Alias" plugin!'));
 	}
 }
