65cf86e9233b9fe42d60848c1a0c30e5831bd5e0
reactomebot <reactome@gmail.com>
2019-11-22 03:00:11 +0000

    website2github.sh automated commit on 2019-11-22 03:00:01 UTC

--- plugins/system/jce/js/media.js
+++ plugins/system/jce/js/media.js
@@ -1,2 +1,2 @@
-/* jce - 2.7.19 | 2019-10-25 | https://www.joomlacontenteditor.net | Copyright (C) 2006 - 2019 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
+/* jce - 2.7.20 | 2019-11-20 | https://www.joomlacontenteditor.net | Copyright (C) 2006 - 2019 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
 !function($){$(document).ready(function($){$(".wf-media-input").not(".wf-media-input-readonly").removeAttr("readonly"),$(document).on("subform-row-add",function(event,row){$(row).find(".wf-media-input").not(".wf-media-input-readonly").removeAttr("readonly")})})}(jQuery);
