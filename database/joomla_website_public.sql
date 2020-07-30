dc437595f93083e423e8b376db8a2b0adf3c48f0
reactomebot <reactome@gmail.com>
2020-07-30 03:00:05 +0000

    website2github.sh automated commit on 2020-07-30 03:00:01 UTC

--- database/joomla_website_public.sql
+++ database/joomla_website_public.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
 --
 -- Host: localhost    Database: website
 -- ------------------------------------------------------
--- Server version	5.7.30-0ubuntu0.18.04.1
+-- Server version	5.7.31-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
