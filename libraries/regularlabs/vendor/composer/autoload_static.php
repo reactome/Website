26df926451812afef8ada87b5ed962d6fabd8eea
reactomebot <reactome@gmail.com>
2020-10-27 03:00:07 +0000

    website2github.sh automated commit on 2020-10-27 03:00:01 UTC

--- libraries/regularlabs/vendor/composer/autoload_static.php
+++ libraries/regularlabs/vendor/composer/autoload_static.php
@@ -6,25 +6,26 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitf9099d81d2e2cf4863a68cf73354cfc1
 {
-	public static $prefixLengthsPsr4 = [
-		'R' =>
-			[
-				'RegularLabs\\Library\\' => 20,
-			],
-	];
+    public static $prefixLengthsPsr4 = array (
+        'R' => 
+        array (
+            'RegularLabs\\Library\\' => 20,
+        ),
+    );
 
-	public static $prefixDirsPsr4 = [
-		'RegularLabs\\Library\\' =>
-			[
-				0 => __DIR__ . '/../..' . '/src',
-			],
-	];
+    public static $prefixDirsPsr4 = array (
+        'RegularLabs\\Library\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
 
-	public static function getInitializer(ClassLoader $loader)
-	{
-		return \Closure::bind(function () use ($loader) {
-			$loader->prefixLengthsPsr4 = ComposerStaticInitf9099d81d2e2cf4863a68cf73354cfc1::$prefixLengthsPsr4;
-			$loader->prefixDirsPsr4    = ComposerStaticInitf9099d81d2e2cf4863a68cf73354cfc1::$prefixDirsPsr4;
-		}, null, ClassLoader::class);
-	}
+    public static function getInitializer(ClassLoader $loader)
+    {
+        return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitf9099d81d2e2cf4863a68cf73354cfc1::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitf9099d81d2e2cf4863a68cf73354cfc1::$prefixDirsPsr4;
+
+        }, null, ClassLoader::class);
+    }
 }
--- media/regularlabs/js/script.js
+++ media/regularlabs/js/script.js
@@ -1,6 +1,6 @@
 /**
  * @package         Regular Labs Library
- * @version         20.10.11720
+ * @version         20.10.18795
  * 
  * @author          Peter van Westen <info@regularlabs.com>
  * @link            http://www.regularlabs.com
@@ -12,11 +12,11 @@
 
 if (typeof window.RegularLabsScripts === 'undefined'
 	|| typeof RegularLabsScripts.version === 'undefined'
-	|| RegularLabsScripts.version < '20.10.11720') {
+	|| RegularLabsScripts.version < '20.10.18795') {
 
 	(function($) {
 		window.RegularLabsScripts = {
-			version: '20.10.11720',
+			version: '20.10.18795',
 
 			ajax_list        : [],
 			started_ajax_list: false,
