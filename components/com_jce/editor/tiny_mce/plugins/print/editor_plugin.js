f9be5882e5688ddaa1cc3fede4d87f83658c674f
adamjohnwright <reactome@gmail.com>
2022-09-28 03:00:08 +0000

    website2github.sh automated commit on 2022-09-28 03:00:01 UTC

--- components/com_jce/editor/tiny_mce/plugins/print/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/print/editor_plugin.js
@@ -1,2 +1,2 @@
-/* jce - 2.9.29 | 2022-08-09 | https://www.joomlacontenteditor.net | Copyright (C) 2006 - 2022 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
+/* jce - 2.9.30 | 2022-09-14 | https://www.joomlacontenteditor.net | Copyright (C) 2006 - 2022 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
 !function(){tinymce.PluginManager.add("print",function(ed){ed.addCommand("mcePrint",function(){ed.getWin().print()}),ed.addButton("print",{title:"print.desc",cmd:"mcePrint"})})}();
