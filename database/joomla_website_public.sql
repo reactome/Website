6d8eacbe27f5ea641f660f73678e2bc96170d6a7
adamjohnwright <reactome@gmail.com>
2024-02-02 03:00:08 +0000

    website2github.sh automated commit on 2024-02-02 03:00:01 UTC

--- database/joomla_website_public.sql
+++ database/joomla_website_public.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
 -- Host: localhost    Database: website
 -- ------------------------------------------------------
--- Server version	8.0.35-0ubuntu0.20.04.1
+-- Server version	8.0.36-0ubuntu0.20.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
